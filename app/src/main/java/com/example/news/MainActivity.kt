7c7a8fc64ba8b161ab45a0974fd65506dcf54642
Semiletov Andrey <semiletovandrey15@gmail.com>
2023-04-21 13:55:17 +0300

    1 зажание

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         val newsList = listOf(
             TheNews(
                 R.drawable.bmw,
-                "BMW does not surrender the niche and has the status of a luxury car ",
+                R.string.BMWNewsTitle.toString(),
                 "После долгого ожидания, финальная часть эпической саги о Мстителях наконец-то выходит на экраны кинотеатров. Фильм обещает быть одним из самых зрелищных и захватывающих фильмов года.",
             0),
             TheNews(
--- app/src/main/java/com/example/news/TheNews.kt
+++ app/src/main/java/com/example/news/TheNews.kt
@@ -1,5 +1,8 @@
 package com.example.news
 
+import android.content.Intent
 import android.graphics.Bitmap
+import android.media.Image
+import android.widget.Button
 
 class TheNews(val imageRes: Int,val title:String, val description:String, val count_like:Int)
